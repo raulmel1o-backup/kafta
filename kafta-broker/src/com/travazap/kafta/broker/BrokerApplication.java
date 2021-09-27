361166be8966cad64f0c12e43c053cfa5d97eb13
Raul Mello <raul.mello@ppro.com>
2021-09-27 19:00:28 -0300

    Fix

--- kafta-broker/src/com/travazap/kafta/broker/BrokerApplication.java
+++ kafta-broker/src/com/travazap/kafta/broker/BrokerApplication.java
@@ -1,7 +1,13 @@
 package com.travazap.kafta.broker;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class BrokerApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("broker");
+
+        ServerSocket serverSocket = new ServerSocket(6666);
+        serverSocket.accept();
     }
 }
--- kafta-producer/src/com/travazap/kafta/producer/ProducerApplication.java
+++ kafta-producer/src/com/travazap/kafta/producer/ProducerApplication.java
@@ -1,7 +1,24 @@
 package com.travazap.kafta.producer;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class ProducerApplication {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         System.out.println("producer");
+
+        Socket socket = new Socket("localhost", 6666);
+
+        OutputStream outputStream = socket.getOutputStream();
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+
+        dataOutputStream.writeBytes("Hello Trava");
+        dataOutputStream.flush();
+        dataOutputStream.close();
+
+        socket.close();
     }
 }
