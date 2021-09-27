20c69126150978a724c565c18e174217e056ea34
Raul Mello <raul.mello@ppro.com>
2021-09-27 18:01:09 -0300

    Add main

---
+++ kafta-broker/src/com/travazap/kafta/broker/BrokerApplication.java
@@ -0,0 +1,7 @@
+package com.travazap.kafta.broker;
+
+public class BrokerApplication {
+    public static void main(String[] args) {
+        System.out.println("broker");
+    }
+}
---
+++ kafta-producer/src/com/travazap/kafta/producer/ProducerApplication.java
@@ -0,0 +1,7 @@
+package com.travazap.kafta.producer;
+
+public class ProducerApplication {
+    public static void main(String[] args) {
+        System.out.println("producer");
+    }
+}
