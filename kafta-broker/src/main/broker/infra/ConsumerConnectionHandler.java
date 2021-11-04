0a6ef841d210d4df20dc08a5a027c5c83594e167
Raul Mello <raul.mello@ppro.com>
2021-11-03 22:44:37 -0300

    remove unused import

--- kafta-broker/src/main/broker/infra/ConsumerConnectionHandler.java
+++ kafta-broker/src/main/broker/infra/ConsumerConnectionHandler.java
@@ -10,7 +10,6 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class ConsumerConnectionHandler extends Thread {
 
